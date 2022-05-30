6b40027411ca8c9e9ce2f47cc829be53a7e15fa4
BarbaraC12 <cano.barbara@outlook.fr>
2022-05-30 20:59:44 +0200

    push home

---
+++ vector.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 17:01:31 by bcano             #+#    #+#             */
+/*   Updated: 2022/05/29 17:38:04 by bcano            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <iostream>
+
+namespace ft {
+
+    template <class T, class ALlocator = std::allocator<T> >
+    class vector {
+
+        public :
+
+            /* MEMBER TYPES*/
+            typedef T
+            typedef Allocator
+            
+            /* MEMBER FUNCTIONS*/
+                // Iterators
+                // Capacity
+                // Element access
+                // Modifiers
+                // Allocators
+            /* NON-MEMBER FUNCTION OVERLOADS*/
+    }
+}
+
+
+#ifndef
