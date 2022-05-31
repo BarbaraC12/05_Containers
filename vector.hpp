02c5e586dff50795c50b995348f3955ded2f7c3d
Barbara CANO <bcano@e1r8p21.42.fr>
2022-05-31 23:42:13 +0200

    no

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: bcano <bcano@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 17:01:31 by bcano             #+#    #+#             */
-/*   Updated: 2022/05/29 17:38:04 by bcano            ###   ########.fr       */
+/*   Updated: 2022/05/31 22:24:24 by bcano            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,34 @@
 
 namespace ft {
 
-    template <class T, class ALlocator = std::allocator<T> >
-    class vector {
+	template <class T, class ALlocator = std::allocator<T> >
+	class vector {
 
-        public :
+		public :
 
-            /* MEMBER TYPES*/
-            typedef T
-            typedef Allocator
-            
-            /* MEMBER FUNCTIONS*/
-                // Iterators
-                // Capacity
-                // Element access
-                // Modifiers
-                // Allocators
-            /* NON-MEMBER FUNCTION OVERLOADS*/
-    }
+			/* MEMBER TYPES*/
+			typedef T			value_type;
+			typedef Alloc		allocator_type;
+			typedef std::size_t	size_type; // Unsigned integer type (usually std::size_t)
+			typedef ptrdiff_t	difference_type;// Signed integer type (usually std::ptrdiff_t)
+			typedef &T			reference;	// value_type&
+			typedef const &T	const_reference	// const value_type&
+			// pointer Allocator::pointer	(until C++11)
+			// const_pointer    Allocator::const_pointer	(until C++11)
+			// iterator     LegacyRandomAccessIterator and LegacyContiguousIterator to value_type   (until C++20)
+			// const_iterator   LegacyRandomAccessIterator and LegacyContiguousIterator to const value_type  (until C++20)
+			// reverse_iterator     std::reverse_iterator<iterator>
+			// const_reverse_iterator	std::reverse_iterator<const_iterator>
+			
+			/* MEMBER FUNCTIONS*/
+				// Iterators
+				// Capacity
+				// Element access
+				// Modifiers
+				// Allocators
+			/* NON-MEMBER FUNCTION OVERLOADS*/
+	};
 }
 
 
-#ifndef
+#endif
